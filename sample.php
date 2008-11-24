17e876c3417324560bd071d92a56b7bd376f4869
Dave Brondsema <dave@brondsema.net>
2008-11-24 13:03:21 -0500

    sample with callback func

--- sample.php
+++ sample.php
@@ -1,11 +1,19 @@
 <?php
 require_once dirname(__FILE__) . '/phpAjaxCalendar.inc.php';
 
+# using a callback function like this is optional
+function day_details($y,$m,$d) {
+    echo "$d";
+    if ($m == 1 and $d == 1) {
+        echo "<br/><em>New Year's Day!</em>";
+    }
+}
+
 if(@$_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest' && isset($_GET['month']))
 {
     $month = $_GET['month'];
     $year = isset($_GET['year']) ? $_GET['year'] : date('Y');
-    die(getPhpAjaxCalendarCore($month,$year));
+    die(getPhpAjaxCalendarCore($month,$year,'day_details'));
 }
 ?>
 <html>
@@ -23,7 +31,7 @@ $year = date('Y');
 if (isset($_GET['year'])) {
     $year = $_GET['year'];
 }
-echo getPhpAjaxCalendar($month,$year);
+echo getPhpAjaxCalendar($month,$year,'day_details');
 ?>
 </body>
 </html>
